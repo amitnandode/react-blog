47b932a80bfe06cc06ed8a5319b152c990a56ccf
Amit Nandode <nandodeamit@gmail.com>
2023-12-29 23:13:50 +0530

    used createContext hook for prop sharing

--- src/EditPost.js
+++ src/EditPost.js
@@ -1,17 +1,36 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
+import { useContext } from "react";
+import DataContext from "./context/DataContext";
+import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
+import api from "./api/posts";
 
-const EditPost = ({
-  posts,
-  handleEdit,
-  editBody,
-  setEditBody,
-  editTitle,
-  setEditTitle,
-}) => {
+const EditPost = () => {
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
+  const { posts, setPosts } = useContext(DataContext);
   const { id } = useParams();
   const post = posts.find((post) => post.id.toString() === id);
+
+  const history = useNavigate();
+
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(
+        posts.map((post) => (post.id === id ? { ...response.data } : post))
+      );
+      setEditTitle("");
+      setEditBody("");
+      history("/");
+    } catch (err) {
+      console.log(`Error ${err.message}`);
+    }
+  };
 
   useEffect(() => {
     if (post) {
--- src/PostPage.js
+++ src/PostPage.js
@@ -1,9 +1,28 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "./api/posts";
+import DataContext from "./context/DataContext";
 
-const PostPage = ({ posts, handleDelete }) => {
+const PostPage = () => {
+  const { posts, setPosts } = useContext(DataContext);
   const { id } = useParams();
   const post = posts.find((post) => post.id.toString() === id);
+
+  const history = useNavigate();
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const postList = posts.filter((post) => post.id !== id);
+      setPosts(postList);
+      history("/");
+    } catch (err) {
+      console.log(`Error ${err.message}`);
+    }
+  };
+
   return (
     <main className="PostPage">
       <article className="post">
